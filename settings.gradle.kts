55bbdd51a08d953362676440807d3984d2b1c862
Chaitanya Reddy <109501392+chaitanyaa69@users.noreply.github.com>
2024-01-06 11:33:32 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "21501a05f0"
+include(":app")
+ 
